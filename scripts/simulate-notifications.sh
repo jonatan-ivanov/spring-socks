cd89fa6e4b14a73bb099fc887ceb665684b066a8
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-07-16 16:01:59 -0700

    simulate notifications

---
+++ scripts/simulate-notifications.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# https://github.com/variadico/noti
+
+sleep 5
+echo '' | noti -t 'Issue Created' -m 'Socks App'
+sleep 3
+echo '' | noti -t 'Issue Created' -m 'Socks App'
+echo '' | noti -t 'Issue Created' -m 'Socks App'
+sleep 2
+echo '' | noti -t 'Issue Created' -m 'Socks App'
+echo '' | noti -t 'Issue Created' -m 'Socks App'
+echo '' | noti -t 'Issue Created' -m 'Socks App'
+sleep 1
+echo '' | noti -t 'Issue Created' -m 'Socks App'
+echo '' | noti -t 'Issue Created' -m 'Socks App'
+echo '' | noti -t 'Issue Created' -m 'Socks App'
+echo '' | noti -t 'Issue Created' -m 'Socks App'
+echo '' | noti -t 'Issue Created' -m 'Socks App'
+echo '' | noti -t 'Issue Created' -m 'Socks App'
+echo '' | noti -t 'Issue Created' -m 'Socks App'
+echo '' | noti -t 'Issue Created' -m 'Socks App'
