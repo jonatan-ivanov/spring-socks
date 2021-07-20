62c277c5dcaf8ad6b643dbcacb91386464444b23
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-07-19 17:02:35 -0700

    progress dots to load generator scripts

--- scripts/generate-ko-load.sh
+++ scripts/generate-ko-load.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-while true; do sleep 0.2 && curl -s 'http://localhost/' > /dev/null; done
+while true; do
+    sleep 0.2 && curl -s 'http://localhost/' > /dev/null
+    echo -n '.'
+done
--- scripts/generate-ok-load.sh
+++ scripts/generate-ok-load.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-while true; do sleep 0.2 && curl -s 'http://localhost/?devMode=false' > /dev/null; done
+while true; do
+    sleep 0.2 && curl -s 'http://localhost/?devMode=false' > /dev/null
+    echo -n '.'
+done
