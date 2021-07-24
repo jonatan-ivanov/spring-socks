65b25855fd584c4bd8f52903282dafc067711e5c
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-07-23 17:15:13 -0700

    fixing timezone in fake log generator

--- scripts/generate-fake-log-files.sh
+++ scripts/generate-fake-log-files.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
 
-mkdir -p fake-logs
+mkdir -p fake-logs/content
 
 for i in {0..9}; do
     if [ "$((i % 2))" == 0 ]; then
-        touch "fake-logs/cart-api.2021-08-31T02:0$i:00Z.tar.gz"
-        touch "fake-logs/catalog-api.2021-08-31T02:0$i:00Z.tar.gz"
-        touch "fake-logs/order-api.2021-08-31T02:0$i:00Z.tar.gz"
-        touch "fake-logs/payment-api.2021-08-31T02:0$i:00Z.tar.gz"
-        touch "fake-logs/shipping-api.2021-08-31T02:0$i:00Z.tar.gz"
-        touch "fake-logs/user-api.2021-08-31T02:0$i:00Z.tar.gz"
-        touch "fake-logs/shop-ui.2021-08-31T02:0$i:00Z.tar.gz"
+        touch "fake-logs/content/cart-api.2021-09-02T02:0$i:00Z.tar.gz"
+        touch "fake-logs/content/catalog-api.2021-09-02T02:0$i:00Z.tar.gz"
+        touch "fake-logs/content/order-api.2021-09-02T02:0$i:00Z.tar.gz"
+        touch "fake-logs/content/payment-api.2021-09-02T02:0$i:00Z.tar.gz"
+        touch "fake-logs/content/shipping-api.2021-09-02T02:0$i:00Z.tar.gz"
+        touch "fake-logs/content/user-api.2021-09-02T02:0$i:00Z.tar.gz"
+        touch "fake-logs/content/shop-ui.2021-09-02T02:0$i:00Z.tar.gz"
     fi
 done
 
-touch "fake-logs/cart-api.log"
-touch "fake-logs/catalog-api.log"
-touch "fake-logs/order-api.log"
-touch "fake-logs/payment-api.log"
-touch "fake-logs/shipping-api.log"
-touch "fake-logs/user-api.log"
-touch "fake-logs/shop-ui.log"
+touch "fake-logs/content/cart-api.log"
+touch "fake-logs/content/catalog-api.log"
+touch "fake-logs/content/order-api.log"
+touch "fake-logs/content/payment-api.log"
+touch "fake-logs/content/shipping-api.log"
+touch "fake-logs/content/user-api.log"
+touch "fake-logs/content/shop-ui.log"
